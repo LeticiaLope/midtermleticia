54f1b8ea632ae988e0144030d8696d87cf4d115c
LeticiaLope <leticia_daniela@live.com.mx>
2020-10-29 22:49:17 -0400

    created atlas db and connected to config/db

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 //<!--db.js, Leticia Lopez, 301087698, midterm-->
 module.exports=
 {
-    "URI": "mongodb://localhost/books"
+    //"URI": "mongodb://localhost/books" //v3 commented
     //v2 commented
-//"URI": "mongodb+srv://daniela:HMBkfCxuHCu4TH5s@mongodbserver.7dqi0.mongodb.net/dbnamehere?retryWrites=true&w=majority",
+"URI": "mongodb+srv://leticia:xbeqJwL62K6QjBq9@mongodbserver.0inwe.mongodb.net/book_store?retryWrites=true&w=majority"
     //v2 commented
 //"Secret": 'SomeSecret'
 }
